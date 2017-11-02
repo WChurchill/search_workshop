6b00c3fdfd6926f97d2c5fcbfee8e831e4b5daec
Michael Hollister <michael.s.hollister@gmail.com>
2017-11-02 05:41:51 -0500

    Implemented alpha-beta pruning

--- src/minimax.py
+++ src/minimax.py
@@ -9,23 +9,41 @@ class SearchNode():
         self.board = board
 
 
-def minimax(current_node, maximize=True):
+def minimax(current_node, a, b, maximize=True):
     # check if we're at a leaf node
     util = utility(current_node.board)
     if util is not None:
         # the game is over, return the reward
         display(current_node.board)
         return SearchNode(util, current_node.board)
-    elif maximize:
+
+    if maximize:
+        child_node = SearchNode(-9999999999, current_node.board)
+
         # if it's our turn, return the move that maximizes utility
-        child_nodes = [
-            minimax(SearchNode(None, state), not maximize)
-            for state in successors(current_node.board, 'X')
-        ]
-        return max(child_nodes, key=lambda node: node.utility)
+        for state in successors(current_node.board, 'X'):
+            child_node = max(child_node, minimax(SearchNode(None, state), a, b, not maximize),
+                             key=lambda node: node.utility)
+
+            # Alpha-Beta pruning
+            if child_node.utility >= b:
+                return child_node
+
+            a = max(a, child_node.utility)
+
+        return child_node
     else:
-        child_nodes = [
-            minimax(SearchNode(None, state), not maximize)
-            for state in successors(current_node.board, 'X')
-        ]
-        return min(child_nodes, key=lambda node: node.utility)
+        child_node = SearchNode(9999999999, current_node.board)
+
+        # If opponent's turn, then minimize 
+        for state in successors(current_node.board, 'O'):
+            child_node = min(child_node, minimax(SearchNode(None, state), a, b, not maximize),
+                             key=lambda node: node.utility)
+
+            # Alpha-Beta pruning
+            if child_node.utility <= a:
+                return child_node
+
+            b = min(b, child_node.utility)
+
+        return child_node
