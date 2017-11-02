7e690f8aa096c8ea690a35ba77cd08fdaa42b8db
Winston Carlile <winston.carlile@gmail.com>
2017-11-02 03:50:48 -0500

    Use heapq with (cost, state) tuples

--- src/generic.py
+++ src/generic.py
@@ -1,11 +1,15 @@
 from heapq import heappush, heappop
-from graph import goal_test, successors, start_node
+from graph import goal_test, successors, start_state
 
-current_node = start_node  # we are given the starting state
+current_state = start_state  # we are given the starting state
+cost = 0
 history = set()  # keep track of nodes already visited
-queue = []  #
-while not goal_test(current_node):
-    for child in successors(current_node):
+heap = []  #
+current_node = (cost, current_state)
+heappush(heap, current_node)
+while not goal_test(current_node[1]):
+    for child in successors(current_state):
         if child not in history:
-            heappush(queue, child)  # add nodes to list
-    current_node = heappop(queue)  # go to the next state
+            heappush(heap, (cost, child))  # add nodes to list
+            set.add(child)
+    current_node = heappop(heap)  # go to the next state
