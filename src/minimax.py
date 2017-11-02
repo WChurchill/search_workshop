a72a67373bb1a385c576b95830062e86e5ab17b0
Winston Carlile <winston.carlile@gmail.com>
2017-11-02 03:51:31 -0500

    Implement minimax with SearchNode objects

--- src/minimax.py
+++ src/minimax.py
@@ -1,17 +1,31 @@
 # Minimax Search with alpha-beta pruning
 
-import random
-from tictactoe import successors, utility
+from tictactoe import successors, utility, display
 
 
-def minimax(state, maximize=True):
+class SearchNode():
+    def __init__(self, utility, board):
+        self.utility = utility
+        self.board = board
+
+
+def minimax(current_node, maximize=True):
     # check if we're at a leaf node
-    if leaf(state):
-        return utility(state)
+    util = utility(current_node.board)
+    if util is not None:
+        # the game is over, return the reward
+        display(current_node.board)
+        return SearchNode(util, current_node.board)
+    elif maximize:
+        # if it's our turn, return the move that maximizes utility
+        child_nodes = [
+            minimax(SearchNode(None, state), not maximize)
+            for state in successors(current_node.board, 'X')
+        ]
+        return max(child_nodes, key=lambda node: node.utility)
     else:
-        return max(children, key=lambda x: x[0])
-
-
-def utility(state):
-    """Evaluates the utility of a game state"""
-    return 0
+        child_nodes = [
+            minimax(SearchNode(None, state), not maximize)
+            for state in successors(current_node.board, 'X')
+        ]
+        return min(child_nodes, key=lambda node: node.utility)
