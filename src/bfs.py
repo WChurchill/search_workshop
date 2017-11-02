3af6cf80d6ef6653d1a2b54840b27358c16ed192
Winston Carlile <winston.carlile@gmail.com>
2017-11-02 03:50:21 -0500

    Hook up breadth-first-search with maze generator

--- src/bfs.py
+++ src/bfs.py
@@ -1,16 +1,33 @@
 from Maze import Maze
 
-maze = Maze(30, 30)
+
+class SearchNode():
+    def __init__(self, state, parent):
+        self.state = state
+        self.parent = parent
+
+
+maze = Maze(4, 6)
+maze.print()
+print()
 goal_test = maze.getTerminalFunc()
 successors = maze.getSuccessorFunc()
-start_node = (0, 0)
+start_node = SearchNode((0, 0), None)
 
 current_node = start_node  # we are given the starting state
 history = set()  # keep track of nodes already visited
 queue = []  # use python list as queue
-while not goal_test(current_node):
-    history.add(current_node)
-    for child in successors(current_node):
-        if child not in history:
-            queue.append(child)  # add nodes to list
+while not goal_test(current_node.state):
+    history.add(current_node.state)
+    for new_state in successors(current_node.state):
+        if new_state not in history:
+            queue.append(SearchNode(new_state,
+                                    current_node))  # add nodes to list
     current_node = queue.pop()  # go to the next state
+
+path = set(current_node.state)
+while current_node.parent:
+    current_node = current_node.parent
+    path.add(current_node.state)
+
+# maze.print(path)
